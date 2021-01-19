22e2e6abdcb433328cb63f6c69f7e1dbed5015a5
ValeriaMahdenko <70704714+ValeriaMahdenko@users.noreply.github.com>
2021-01-19 20:31:38 +0200

    Create Validation.py

---
+++ Mega Casino/Validation.py
@@ -0,0 +1,20 @@
+class Validation:
+    @staticmethod
+    def validate_string(func):
+        def stringWrapper(self, value):
+            if any(map(str.isdigit, value)):
+                raise ValueError("No integers in string!")
+            return func(self, value)
+        return stringWrapper
+
+    @staticmethod
+    def validate_number(func):
+        def numberWrapper(self, value):
+            try:
+                value = int(value)
+            except ValueError:
+                raise ValueError("No letters/characters in number!")
+            if int(value) < 0:
+                raise ValueError("Number must be > 0")
+            return func(self, value)
+        return numberWrapper
