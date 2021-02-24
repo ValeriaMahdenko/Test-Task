d969ef792a37fb2a520d1462abf41057400b4266
Valeria Mahdenko <vumahdenko6@gmail.com>
2021-02-24 15:32:52 +0200

    Change Validation.py

--- Validation.py
+++ Validation.py
@@ -1,9 +1,21 @@
-aclass Validation:
-        @staticmethod
-            def validate_string(func):
-                        def stringWrapper(self, value):
-                                        if any(map(str.isdigit, value)):
-                                                            raise ValueError("No integers in string!")
-                                                                    return func(self, value)
-                                                                        return stringWrapper
+class Validation:
+    @staticmethod
+    def validate_string(func):
+        def stringWrapper(self, value):
+            if any(map(str.isdigit, value)):
+                raise ValueError("No integers in string!")
+            return func(self, value)
+        return stringWrapper
+
+    @staticmethod
+    def validate_number(func):
+        def numberWrapper(self, value):
+            try:
+                value = int(value)
+            except ValueError:
+                raise ValueError("No letters/characters in number!")
+            if int(value) < 0:
+                raise ValueError("Number must be > 0")
+            return func(self, value)
+        return numberWrapper
 
